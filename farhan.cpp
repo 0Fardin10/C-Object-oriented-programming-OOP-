31c0f40c09e9e1dc54306d8b8d66e7f6473c9fcd
0Fardin10 <fardinmd268@gmail.com>
2026-04-29 23:18:53 +0600

    cpp

---
+++ farhan.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+int a=50;
+
+  float formula = (a-32)*5/9;
+  std:: cout<<formula;
+return 0;
+}
---
+++ mobum.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"my mobile number is"
+        "0\a1\a2\a"
+        return 0;
+}
