2ef64d9c06ed8c7d8105a5dcfdf84d104e9d8745
0Fardin10 <fardinmd268@gmail.com>
2026-05-11 14:51:39 +0600

    guessing number

---
+++ guessingGame.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main(){
+  while(1){
+  int picNumber,randomNum;
+  cout<<"Guess a random number between 1 to 5";
+  cin>>picNumber;
+    randomNum=1+rand()%5;
+    if (picNumber==randomNum) {
+      cout<<"You won"<<endl<<endl;
+    }
+    else {
+      cout<<"You lost ... try again"<<endl<<endl;
+      cout<<"Random number was : "<<randomNum<<endl;
+    }
+  }
+
+
+}
---
+++ randomnub.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(){
+  for (int i = 1; i <=10; i++) {
+
+      int randomNum=rand()%10;                        
+  cout<<"random number is = "<<randomNum<<endl;
+
+  }
+
+}
