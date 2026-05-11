121187a877fe849cdf8bed2df5c6ceaac0397411
0Fardin10 <fardinmd268@gmail.com>
2026-05-11 13:04:36 +0600

    function end

---
+++ advantagesofparameters.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void suqare(int n){
+  int result=n*n;
+  cout<<"Suqare  result is : "<<result<<endl;
+}
+int main(){
+suqare(8);
+suqare(5);
+return 0;
+}
