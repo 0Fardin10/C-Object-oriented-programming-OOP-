8f1ac7a96e260e2fcfb97ecf08b76d9d422d8a7e
0Fardin10 <fardinmd268@gmail.com>
2026-05-24 12:52:09 +0600

    class function

---
+++ addingFunction.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class student 
+{
+  public:
+int id ;
+double gpa;
+};
+
+int main (){
+  student Alim;
+  Alim.id=4637;
+  Alim.gpa=3.44;
+  cout<<Alim.id<<endl<<Alim.gpa<<endl;
+}
