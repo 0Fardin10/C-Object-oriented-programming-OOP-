06809ca32c3df1ed93b55f8cf1fdda92773d0db3
0Fardin10 <fardinmd268@gmail.com>
2026-05-29 23:19:03 +0600

    ccc

--- 01.cpp
+++ 01.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
+#include "myclass.h"
+
 using namespace std;
-int main (){
-  cout<<"hello \a";
+
+int main(){
+    MyClass o1;
+    return 0;
 }
+
---
+++ myclass.cpp
@@ -0,0 +1,8 @@
+#include "myclass.h"
+#include<iostream>
+using namespace std;
+MyClass::MyClass()
+{
+    count<<"Inside constructor"<<endl;
+    return 0;
+}
